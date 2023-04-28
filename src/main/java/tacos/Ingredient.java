f4694b1544b99ef5fe8b41744607403fc0d72f5e
KebronDaniel <kebronjrdaniel@gmail.com>
2023-04-28 16:57:18 +0300

    Implemented the controller class and put a separate enum class for the IngredientType .Closes #2

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -7,9 +7,5 @@ public class Ingredient {
 
     private final String id;
     private final String name;
-    private final Type type;
-
-    public enum Type {
-        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
-    }
+    private final IngredientType type;
 }
