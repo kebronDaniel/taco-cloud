f889535cdb405999f49300984e867bee49a4e470
KebronDaniel <kebronjrdaniel@gmail.com>
2024-04-21 17:25:40 +0300

    changes in the repo so that they use type int in the id

--- src/main/java/tacos/Repositories/TacoOrderRepo.java
+++ src/main/java/tacos/Repositories/TacoOrderRepo.java
@@ -6,9 +6,9 @@ import tacos.Domain.TacoOrder;
 import java.util.Date;
 import java.util.List;
 
-public interface TacoOrderRepo extends CrudRepository<TacoOrder, String> {
+public interface TacoOrderRepo extends CrudRepository<TacoOrder, Integer> {
 
     List<TacoOrder> findByDeliveryZip(String DeliveryZip);
-    List<TacoOrder> findByDeliveryZipAndPlacedAtBetween(String DeliveryZip, Date start, Date end);
+//    List<TacoOrder> findByDeliveryZipAndPlacedAtBetween(String DeliveryZip, Date start, Date end); // use this after you add the placedat field in the entity.
     List<TacoOrder> findByDeliveryZipAndDeliveryStateOrderByDeliveryCityAllIgnoreCase(String DeliveryTo, String DeliveryState);
 }
--- src/main/java/tacos/Repositories/TacoRepo.java
+++ src/main/java/tacos/Repositories/TacoRepo.java
@@ -3,6 +3,5 @@ package tacos.Repositories;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Domain.Taco;
 
-public interface TacoRepo extends CrudRepository<Taco, String> {
-
+public interface TacoRepo extends CrudRepository<Taco, Integer> {
 }
