60be00b32ad58a1378924b402bffd46055b502e7
KebronDaniel <kebronjrdaniel@gmail.com>
2024-05-06 10:44:48 +0300

    Added a sec config file to handle authorization and login functionalities.

--- src/main/java/tacos/Configuration/SecurityConfig/SecurityConfig.java
+++ src/main/java/tacos/Configuration/SecurityConfig/SecurityConfig.java
@@ -2,14 +2,31 @@ package tacos.Configuration.SecurityConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests-> requests.requestMatchers("/design","/orders").authenticated()
+                        .requestMatchers("/","/**").permitAll()
+                        .anyRequest().permitAll())
+                .formLogin(login -> login.loginPage("/login").permitAll());
+        return http.build();
+
+        // when we direct the process to authenticate the user,
+        // spring Security's UsernamePasswordAuthenticationFilter will intercept the request to /authenticate and initiate the authentication process using the provided credentials.
     }
 }
