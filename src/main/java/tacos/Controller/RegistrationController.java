5fb567f36de0e735253bcbf2cd44a53a756ccce3
KebronDaniel <kebronjrdaniel@gmail.com>
2024-05-06 10:44:02 +0300

    Added error handling logic in the page the controller and the DTO.(But it's unfinished)

--- src/main/java/tacos/Controller/RegistrationController.java
+++ src/main/java/tacos/Controller/RegistrationController.java
@@ -2,30 +2,61 @@ package tacos.Controller;
 
 
 import jakarta.servlet.Registration;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import tacos.DTOs.RegistrationForm;
+import tacos.Domain.Taco;
+import tacos.Domain.User;
+import tacos.Service.RoleService;
 import tacos.Service.UserService;
 
+import java.util.stream.Collectors;
+
+@Slf4j
 @Controller
 @RequestMapping("/register")
+@SessionAttributes("RegistrationForm")
 public class RegistrationController {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    RoleService roleService;
+
+    @ModelAttribute("RegistrationForm")
+    public RegistrationForm registrationForm(Model model){
+        return new RegistrationForm();
+    }
 
     @GetMapping
-    public String register() {
+    public String showRegistrationForm(Model model) {
         return "register";
     }
 
     @PostMapping
-    public String processRegistration(RegistrationForm form) {
-        userService.save(form.toUser());
-        return "redirect:/login";
+    public String processRegistration(@Valid RegistrationForm form, Errors errors, SessionStatus sessionStatus) {
+        if (errors.hasErrors()){
+            log.info("errors" + errors);
+            return "register";
+        }
+        User user = form.toUser();
+        if (userService.findByUsername(user.getUsername()) != null) {
+            System.out.println("User name already exists");
+            return "register";
+        }
+        user.addRole(roleService.findByName("ROLE_USER"));
+        userService.save(user);
+        sessionStatus.setComplete();
+        return "login";
+
     }
+
 }
--- src/main/java/tacos/DTOs/RegistrationForm.java
+++ src/main/java/tacos/DTOs/RegistrationForm.java
@@ -1,27 +1,50 @@
 package tacos.DTOs;
 
-import lombok.Data;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import tacos.Domain.Ingredient;
+import tacos.Domain.Role;
 import tacos.Domain.User;
+import tacos.Service.IngredientTypeService;
+import tacos.Service.IngredientsService;
+import tacos.Service.RoleService;
+
+import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
 public class RegistrationForm {
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    private final String username;
-    private final String password;
-    private final String fullName;
-    private final String street;
-    private final String city;
-    private final String state;
-    private final String zip;
-    private final String phoneNumber;
+
+    @NotBlank(message = "username must not be blank")
+    private String username;
+    @NotBlank(message = "password must not be blank")
+    private String password;
+    @NotBlank(message = "Full Name must not be blank")
+    private String fullName;
+    @NotBlank(message = "Street must not be blank")
+    private String street;
+    @NotBlank(message = "city must not be blank")
+    private String city;
+    @NotBlank(message = "state must not be blank")
+    private String state;
+    @NotBlank(message = "zip must not be blank")
+    private String zip;
+    @Digits(fraction = 0, integer = 10, message = "Invalid Phone Number")
+    private String phoneNumber;
 
     public User toUser(){
-        return new User(username, passwordEncoder.encode(password), fullName, street, city, state, zip, phoneNumber);
+        User user =  new User(username, passwordEncoder.encode(password), fullName, street, city, state, zip, phoneNumber);
+        return user;
     }
 
 }
